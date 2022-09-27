b1efa8d5c5f039df3ec45be130240c8454e77576
Tommy Xu <zwxu@umich.edu>
2022-09-27 14:49:49 -0400

    make change

--- Hello.java
+++ Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
-  public static void main(String[] args)
-    for(int i=1;i<=10;i++){
-      system.out.println("Hello World..."+i);
-    }
+	public static void main(String[] args) {
+
+		for(int i=1;i<=10;i++) {
+
+			System.out.println("Hello World..."+i);
+		}
+	}
 }
